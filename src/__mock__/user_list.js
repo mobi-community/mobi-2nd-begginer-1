85a8f6b59539f1ebdb8f83a522e7e74218aca8b4
HeesikK <7jjhkhz@naver.com>
2023-12-03 14:09:04 +0900

    feat: 페이지네이션 구현 중

--- src/__mock__/user_list.js
+++ src/__mock__/user_list.js
@@ -1,15 +1,57 @@
-export const UserList = (count) => {
-  function generateRandomDate(from, to) {
-    return new Date(from.getTime() + Math.random() * (to.getTime() - from.getTime()));
-  }
+export const UserList = () => {
+  // 랜덤 이름 생성
+  const generateName = () => {
+    const lastName = ["김", "이", "박", "최", "정", "강", "조", "윤", "장", "임", "한", "오", "서", "신", "권", "황", "안", "송", "전", "홍"];
+    const firstName = ["민준", "서윤", "도윤", "서연", "시우", "지우", "지호", "하윤", "지후", "서현", "준서", "하은", "건우", "민서", "우진", "지유", "선우", "윤서", "서진", "채원"];
+    const fullName = lastName[Math.floor(Math.random() * firstName.length)] + firstName[Math.floor(Math.random() * lastName.length)];
+    return fullName;
+  };
 
-  return Array(count)
+  // 랜덤 날짜 생성
+  const generateRandomDate = (from, to) => {
+    return new Date(from.getTime() + Math.random() * (to.getTime() - from.getTime()));
+  };
+
+  // 랜덤 생일 생성
+  const generateBirthday = () => {
+    const date = generateRandomDate(new Date(2023, 0, 1), new Date());
+    const year = date.getFullYear();
+    const month = ("0" + (1 + date.getMonth())).slice(-2);
+    const day = ("0" + date.getDate()).slice(-2);
+    return `${year}-${month}-${day}`;
+  };
+
+  // 랜덤 로그인 시간 생성
+  const generateLastLoginDate = () => {
+    const date = generateRandomDate(new Date(2023, 0, 1), new Date());
+    const year = date.getFullYear();
+    const month = ("0" + (1 + date.getMonth())).slice(-2);
+    const day = ("0" + date.getDate()).slice(-2);
+    const hour = ("0" + date.getHours()).slice(-2);
+    const minute = ("0" + date.getMinutes()).slice(-2);
+    const second = ("0" + date.getSeconds()).slice(-2);
+    return `${year}.${month}.${day}.${hour}:${minute}:${second}`;
+  };
+
+  const generatePhoneNumber = () => {
+    return `010-${Math.floor(Math.random() * 9000) + 1000}-${Math.floor(Math.random() * 9000) + 1000}`;
+  };
+
+  const maskingPhoneNumber = () => {
+    const split = generatePhoneNumber().split("-");
+
+    split[1] = "*".repeat(split[1].length);
+    return split.join("-");
+  };
+
+  return Array(200)
     .fill()
-    .map(() => ({
-      id: Math.floor(Math.random() * 100000),
-      name: "da",
-      birthday: generateRandomDate(new Date(2023, 0, 1), new Date()),
-      number: "a",
-      createdAt: "a",
+    .map((array, index) => ({
+      page: Math.floor(index / 20) + 1,
+      id: index + 1,
+      name: generateName(),
+      birthday: generateBirthday(),
+      number: maskingPhoneNumber(),
+      createdAt: generateLastLoginDate(),
     }));
 };
--- src/pages/manage_member.js
+++ src/pages/manage_member.js
@@ -1,9 +1,133 @@
+import styled from "styled-components";
 import { UserList } from "../__mock__/user_list";
+import { useState } from "react";
 
 const ManageMember = () => {
-  const user = UserList(200);
-  console.log(user);
-  return <>회원 관리 페이지</>;
+  const totalPage = 10;
+  const [users, setUsers] = useState(UserList());
+  const [buttonArray, setButtonArray] = useState([0, 1, 2, 3, 4]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [showUserList, setShowUserList] = useState();
+
+  const handleFirstPage = () => {
+    setButtonArray([0, 1, 2, 3, 4]);
+    setCurrentPage(1);
+  };
+  const handleLastPage = () => {
+    setButtonArray([5, 6, 7, 8, 9]);
+    setCurrentPage(10);
+  };
+
+  const handlePrev = () => {
+    const prevPage = buttonArray.map((value) => {
+      setCurrentPage(currentPage - 1);
+      if (currentPage <= 1) {
+        setCurrentPage(1);
+      }
+      return value - 1;
+    });
+    if (prevPage[0] < 0) return [0, 1, 2, 3, 4];
+    setButtonArray(prevPage);
+  };
+
+  const handleNext = () => {
+    const nextPage = buttonArray.map((value) => {
+      setCurrentPage(currentPage + 1);
+      if (currentPage >= 10) {
+        setCurrentPage(10);
+      }
+      return value + 1;
+    });
+    if (nextPage[nextPage.length - 1] > 9) return [5, 6, 7, 8, 9];
+    setButtonArray(nextPage);
+  };
+
+  const handlePage = (pageButton) => {
+    setCurrentPage(pageButton + 1);
+  };
+
+  console.log(users);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <Th>회원번호</Th>
+            <Th>이름</Th>
+            <Th>생년월일</Th>
+            <Th>전화번호</Th>
+            <Th>로그인시간</Th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr>
+              <div>{user.page}페이지</div>
+              <Td>{user.id}</Td>
+              <Td>{user.name}</Td>
+              <Td>{user.birthday}</Td>
+              <Td>{user.number}</Td>
+              <Td>{user.createdAt}</Td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination>
+        <JumpFirst onClick={handleFirstPage}>{"<<"}</JumpFirst>
+        <Prev onClick={handlePrev}>{"<"}</Prev>
+        {buttonArray.map((pageButton) => (
+          <NumberButton
+            onClick={() => handlePage(pageButton)}
+            style={{
+              backgroundColor: currentPage === pageButton + 1 ? "aliceblue" : "#fff",
+            }}
+          >
+            {pageButton + 1}
+          </NumberButton>
+        ))}
+        <Next onClick={handleNext}>{">"}</Next>
+        <JumpLast onClick={handleLastPage}>{">>"}</JumpLast>
+      </Pagination>
+    </Container>
+  );
 };
 
 export default ManageMember;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+// member table
+const Th = styled.th`
+  padding: 10px;
+`;
+const Td = styled.td`
+  padding: 10px;
+`;
+
+// pagination
+const Pagination = styled.div`
+  /*   display: flex;
+  flex-direction: row; */
+
+  & > button {
+    background-color: white;
+    border: none;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    margin: 0 10px;
+    cursor: pointer;
+
+    &:hover {
+      background-color: aliceblue;
+    }
+  }
+`;
+const JumpFirst = styled.button``;
+const Prev = styled.button``;
+const NumberButton = styled.button``;
+const Next = styled.button``;
+const JumpLast = styled.button``;
